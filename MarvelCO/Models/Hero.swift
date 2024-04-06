8099f01786b75fe6cac0ed60e7198e320134cecd
Geraldo Fernandes <geraldofernandes@gmail.com>
2024-04-06 13:30:56 -0300

    Initial commit

---
+++ MarvelCO/Models/Hero.swift
@@ -0,0 +1,14 @@
+//
+//  Character.swift
+//  MarvelCO
+//
+//  Created by Geraldo Fernandes on 03/04/24.
+//
+
+import Foundation
+
+struct Hero: Codable, Equatable {
+    let name: String
+    let id: Int
+    let url: URL
+}
---
+++ MarvelCO/Models/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  MarvelCO
+//
+//  Created by Geraldo Fernandes on 06/04/24.
+//
+
+import Foundation
